ccf276171ffad7e0972ae14fb52a786ea1c56a41
DenysAptikieiev <rtdan333@gmail.com>
2019-12-15 21:24:18 +0200

    cart commit

--- src/js-app/handlers/subscribeHandler.js
+++ src/js-app/handlers/subscribeHandler.js
@@ -1,14 +1,17 @@
 'use strict';
 
-const defaultPrevent = ($target) => {
-    $target.preventDefault();
+const defaultPrevent = (e, $target) => {
+    e.preventDefault();
     const $inputEmail = global.$main.find('.newsletter__box input');
+    $inputEmail.attr('class', 'input');
     const $value = $inputEmail.val();
     console.log($value);
 
-    /*const correctEmail = email.match(/^[0-9a-z-\.]+\@[0-9a-z-]{2,}\.[a-z]{2,}$/i);
+    const correctEmail = $value.match(/^[0-9a-z-\.]+\@[0-9a-z-]{2,}\.[a-z]{2,}$/i);
     if (!correctEmail) {
+        $inputEmail.attr('placeholder', 'Are you sure your email address is entered correctly?');
+        $inputEmail.css('background', '1px solid red');
         alert('Are you sure your email address is entered correctly?')
         return false
-    }*/
+    }
 };
