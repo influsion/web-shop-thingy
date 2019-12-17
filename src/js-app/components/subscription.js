8b21e6ffcf22bea95f92dd19b46379889d154a78
Alexander Barna <influsion@gmail.com>
2019-12-17 17:49:16 +0200

    added localization of subscribtion

--- src/js-app/components/subscription.js
+++ src/js-app/components/subscription.js
@@ -8,16 +8,16 @@ const subscriptionComponent = () => {
                 <div class="row">
                     <div class="col-lg-7 offset-lg-5 col-md-12 col-12 ptb--150">
                         <div class="section__title text-center">
-                            <h2>Stay With Us</h2>
+                            <h2>${ translate('subscription_title') }</h2>
                         </div>
                         <div class="newsletter__block text-center">
-                            <p>Subscribe to our newsletters now and stay up-to-date with new collections, the latest lookbooks and exclusive offers.</p>
+                            <p>${ translate('subscription_copy') }</p>
                             <form>
                                 <div class="newsletter__box">
                                 <img class="js-warning-icon hide" src="images/icons/warning_input.svg" alt="warning">
-                                <div class="js-warning-text hide">Valid email is required: ex@abc.xyz</div>
-                                    <input type="email" placeholder="Enter your e-mail">
-                                    <button type="button">Subscribe</button>
+                                <div class="js-warning-text hide">${ translate('subscription_input_fail') }</div>
+                                    <input type="email" placeholder="${ translate('subscription_placeholder') }">
+                                    <button type="button">${ translate('subscription_button_text') }</button>
                                 </div>
                             </form>
                         </div>
