14dd43f37fa9d7b063f38d0e2cd9e222060c71a8
Alexander Barna <influsion@gmail.com>
2019-12-19 15:06:09 +0200

    fixed bug of deleting product button

--- src/js/handlers/deleteProductFromBasketHandler.js
+++ src/js/handlers/deleteProductFromBasketHandler.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const deleteProductFromBasketHandler = (e, $target) => {
+    e.preventDefault();
+
     const $cartItem = $target.parents('.js-cart-item');
     const id = getProductIdFromDataSet($cartItem);
 
